f271e816902cfbfbacd9890ec55296d8c37b64af
Fauzan Akmal Mahdi <recovery252@gmail.com>
2024-06-11 12:39:56 +0700

    update sdk to 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.myreadwritefile"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.myreadwritefile"
         minSdk = 25
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
